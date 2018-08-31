8db45b7d0f1eeffd9cf5490d16e6e822f5ecb30d
jrootham <jrootham@gmail.com>
2018-08-31 17:35:54 -0400

    Make a cnf

--- sat/src/sat/core.clj
+++ sat/src/sat/core.clj
@@ -1,7 +1,51 @@
 (ns sat.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.set :refer [intersection difference union]])
+  (:require [clojure.string :as str])
+)
+
+(defn set-add [the-set member]
+	(union the-set (set (list member)))
+)
+
+(defn fill-clause [literals sat-true sat-false]
+	(if (empty? literals)
+
+		(cons sat-true (list sat-false))
+
+		(let 
+			[
+				this-one (first literals)
+				remaining (rest literals)
+			]
+
+			(if (str/starts-with? this-one "~")
+				(fill-clause remaining sat-true (set-add sat-false (subs this-one 1)))
+				(fill-clause remaining (set-add sat-true this-one) sat-false)
+			)
+		)		
+	)
+)
+
+(defn parse-clause [line]
+	(fill-clause (str/split line #" ") (set nil) (set nil))
+)
+
+(defn get-cnf [result]
+	(let [line (read-line)] 
+		(if line
+			(if (or (str/starts-with? line "~ ") (str/blank? line))
+				(get-cnf result)
+				(cons (parse-clause line) (get-cnf result)) 
+			) 
+			result
+		)	
+	)
+)
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "SAT solver"
   [& args]
-  (println "Hello, World!"))
+  (println (get-cnf nil))
+)
+
